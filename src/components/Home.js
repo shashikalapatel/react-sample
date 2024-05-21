1d301f4945e1dfd5dbdeb25d12d3b64788f9658d
Shashikala Patel <shashikala.patel@techment.com>
2024-05-21 13:55:44 +0530

    [Add]- crud operation with firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,81 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
 const Home = () => {
+  const [newName, setNewName] = useState("");
+  const [newMarks, setNewMarks] = useState(0);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    getUsers();
+  });
+
+  const getUsers = async () => {
+    const data = await getDocs(usersCollectionRef);
+    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, {
+      name: newName,
+      marks: Number(newMarks),
+    });
+    getUsers();
+  };
+
+  const updateUser = async (id, marks) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { marks: marks + 1 };
+    await updateDoc(userDoc, newFields);
+    getUsers();
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+    getUsers();
+  };
+
   return (
     <div>
-      <h1>Home</h1>
+      <div className="FormHolder">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Marks..."
+          onChange={(event) => {
+            setNewMarks(event.target.value);
+          }}
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
+      <div className="user-cards">
+        {users.map((user) => (
+          <div key={user.id} className="user-card">
+            <h1>Name: {user.name}</h1>
+            <h1>Marks: {user.marks}</h1>
+            <button onClick={() => updateUser(user.id, user.marks)}>
+              Increase Marks
+            </button>
+            <button onClick={() => deleteUser(user.id)}>Delete User</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
