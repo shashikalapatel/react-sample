b2d162cfc2d80a3348b3a5b2542706b602f642f5
Shashikala Patel <shashikala.patel@techment.com>
2024-05-21 13:32:19 +0530

    [Add]- route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
